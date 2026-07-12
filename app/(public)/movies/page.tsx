5953e4e6f0749dce1cdb94a85cc51d3218452101
DeraTheOz <emmanuelchi545@gmail.com>
2026-07-12 20:14:55 +0100

    feat: update MovieSection import and replace MediaSection with NowPlayingSection and TopRatedSection

--- app/(public)/movies/page.tsx
+++ app/(public)/movies/page.tsx
@@ -1,12 +1,12 @@
 import SearchBar from "@/app/components/forms/search-bar";
-import MediaSection from "@/app/components/media/media-section";
+import MovieSection from "@/app/components/movie/movie-section";
 
 export default function Page() {
   return (
     <div className="space-y-6 md:space-y-8 pb-6">
       <SearchBar placeholder="Search for movies" />
 
-      <MediaSection title="Movies" id="movies-heading" />
+      <MovieSection title="Movies" id="movies-heading" />
     </div>
   );
 }
