193c13fbe9a1d3f363fb2fdf1a7e3aab99b99b28
DeraTheOz <emmanuelchi545@gmail.com>
2026-07-12 18:37:34 +0100

    feat: add sort options for movies and tv-series

---
+++ lib/constants/sort-options.ts
@@ -0,0 +1,23 @@
+import type { FilterSelectOption } from "@/features/movie/hooks/use-filter-select";
+
+export const movieSortOptions: FilterSelectOption[] = [
+  { label: "Most popular", value: "popularity.desc" },
+  { label: "Least popular", value: "popularity.asc" },
+  { label: "Highest rated", value: "vote_average.desc" },
+  { label: "Lowest rated", value: "vote_average.asc" },
+  { label: "Newest", value: "primary_release_date.desc" },
+  { label: "Oldest", value: "primary_release_date.asc" },
+  { label: "Title A–Z", value: "title.asc" },
+  { label: "Title Z–A", value: "title.desc" },
+];
+
+export const tvSortOptions: FilterSelectOption[] = [
+  { label: "Most popular", value: "popularity.desc" },
+  { label: "Least popular", value: "popularity.asc" },
+  { label: "Highest rated", value: "vote_average.desc" },
+  { label: "Lowest rated", value: "vote_average.asc" },
+  { label: "Newest", value: "first_air_date.desc" },
+  { label: "Oldest", value: "first_air_date.asc" },
+  { label: "Title A–Z", value: "name.asc" },
+  { label: "Title Z–A", value: "name.desc" },
+];
