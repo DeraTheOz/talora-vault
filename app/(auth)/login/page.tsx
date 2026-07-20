d910fc6f477a03621452f9e4526666a5d8b69b1a
DeraTheOz <emmanuelchi545@gmail.com>
2026-07-20 20:06:57 +0100

    feat: update page to handle user authentication

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,16 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 import AuthCard from "@/app/components/forms/auth/auth-card";
 import LoginForm from "@/app/components/forms/auth/login-form";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return (
     <AuthCard title="Login">
       <LoginForm />
