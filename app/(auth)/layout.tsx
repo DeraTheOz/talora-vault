12802564b4e7819e7e090f7da6e425b82d91e027
DeraTheOz <emmanuelchi545@gmail.com>
2026-07-23 21:37:44 +0100

    fix: correct import paths and adjust layout for better responsiveness

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/app/components/ui/logo";
+import Logo from "@/app/components/layout/logo";
 
 export default function AuthLayout({
   children,
@@ -7,7 +7,7 @@ export default function AuthLayout({
 }>) {
   return (
     <main className="min-h-dvh bg-talora-dark-blue px-6 py-12 md:px-8">
-      <div className="mx-auto flex min-h-[calc(100dvh-6rem)] w-full max-w-[25rem] flex-col items-center justify-center gap-14">
+      <div className="mx-auto flex min-h-[calc(100dvh-6rem)] w-full max-w-100 flex-col items-center justify-center gap-14">
         <Logo />
         {children}
       </div>
